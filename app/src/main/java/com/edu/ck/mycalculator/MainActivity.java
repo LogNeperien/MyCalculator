df47992767fc3eaffe94ac5f0752c36fdbc376e3
unknown <helene.carlier.gubler@gmail.com>
2020-01-26 13:41:04 +0100

    static fait, dynamic nope

--- app/src/main/java/com/edu/ck/mycalculator/MainActivity.java
+++ app/src/main/java/com/edu/ck/mycalculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.edu.ck.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void myClickHandler(View view) {
+        TextView calcul;
+        calcul = (TextView) findViewById(R.id.calcul);
+
+        TextView result;
+        result = (TextView) findViewById(R.id.result);
+
+        switch (view.getId()) {
+            case R.id.button1:
+                calcul.setText('1');
+                break;
+            case R.id.button2:
+                calcul.setText('2');
+                break;
+            case R.id.button3:
+                calcul.setText('3');
+                break;
+            case R.id.button4:
+                calcul.setText('4');
+                break;
+            case R.id.button5:
+                calcul.setText('5');
+                break;
+            case R.id.button6:
+                calcul.setText('6');
+                break;
+            case R.id.button7:
+                calcul.setText('7');
+                break;
+            case R.id.button8:
+                calcul.setText('8');
+                break;
+            case R.id.button9:
+                calcul.setText('9');
+                break;
+
+        }
+    }
+
 }
